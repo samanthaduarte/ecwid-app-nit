4166bc7b6e2bb1459d2f7c5a7f9a4eb39655c8b4
samanthaduarte <molinajimenez01@gmail.com>
2020-04-13 08:17:45 -0600

    init

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+<!-- Include Ecwid JS SDK -->
+<script src="https://d35z3p2poghz10.cloudfront.net/ecwid-sdk/js/1.2.5/ecwid-app.js"></script>
+
 // Initialize the application
 ec.init({
     app_id: "custom-app-25612030-1", // use your application client_id
@@ -5,16 +8,30 @@ ec.init({
     autoheight: true
   });
   
+var storeData = EcwidApp.getPayload();
+var storeId = storeData.store_id;
+var accessToken = storeData.access_token;
+var language = storeData.lang;
+var viewMode = storeData.view_mode;
+
+if (storeData.public_token !== undefined){
+    var publicToken = storeData.public_token;
+}
+
+if (storeData.app_state !== undefined){
+    var appState = storeData.app_state;
+}
+
 ec.order = ec.order || {};
 ec.order.extraFields = ec.order.extraFields || {};
 
 ec.order.extraFields.nombre = {
     'title': 'Nombre para la factura',
-    'textPlaceholder': 'N',
+    'textPlaceholder': 'Nombre para la factura',
     'type': 'text',
     'tip': 'Nombre o Consumidor final',
     'required': true,
-    'checkoutDisplaySection': 'shipping_address'
+    'checkoutDisplaySection': 'payment_details'
 };
 
 Ecwid.refreshConfig();
@@ -25,7 +42,7 @@ ec.order.extraFields.nit = {
     'type': 'text',
     'tip': 'Ingrese el número de NIT',
     'required': false,
-    'checkoutDisplaySection': 'shipping_address'
+    'checkoutDisplaySection': 'payment_details'
 };
 
 Ecwid.refreshConfig();
@@ -36,7 +53,7 @@ ec.order.extraFields.direccion = {
     'type': 'text',
     'tip': 'Ingrese la dirección para la factura',
     'required': false,
-    'checkoutDisplaySection': 'shipping_address'
+    'checkoutDisplaySection': 'payment_details'
 };
 
 Ecwid.refreshConfig();
