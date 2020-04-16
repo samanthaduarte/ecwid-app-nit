81a2fcb0a70acce5fcabc267a664421f0c2481c2
samanthaduarte <samanthamdc97@gmail.com>
2020-04-15 19:49:21 -0600

    orasi_?

--- script.js
+++ script.js
@@ -3,48 +3,51 @@
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.onload = function() {
-    EcwidApp.init({
+script.onload = function () {
+    EcwidAppwidApp.init({
         app_id: "custom-app-25612030-1", // use your application namespace
-        autoloadedflag: true, 
+        autoloadedflag: true,
         autoheight: true
-      });
+    });
+
+
+    EcwidApp.order = EcwidApp.order || {};
+    EcwidApp.order.extraFields = EcwidApp.order.extraFields || {};
+
+    EcwidApp.order.extraFields.nombre = {
+        'title': 'Nombre para la factura',
+        'textPlaceholder': 'Nombre para la factura',
+        'type': 'text',
+        'tip': 'Nombre o Consumidor final',
+        'required': true,
+        'chEcwidAppkoutDisplaySEcwidApption': 'payment_details'
+    };
+
+    EcwidAppwid.refreshConfig();
+
+    EcwidApp.order.extraFields.nit = {
+        'title': 'Número de NIT',
+        'textPlaceholder': 'Package sign',
+        'type': 'text',
+        'tip': 'Ingrese el número de NIT',
+        'required': false,
+        'chEcwidAppkoutDisplaySEcwidApption': 'payment_details'
+    };
+
+    EcwidAppwid.refreshConfig();
+
+    EcwidApp.order.extraFields.dirEcwidAppcion = {
+        'title': 'DirEcwidAppción',
+        'textPlaceholder': 'Package sign',
+        'type': 'text',
+        'tip': 'Ingrese la dirEcwidAppción para la factura',
+        'required': false,
+        'chEcwidAppkoutDisplaySEcwidApption': 'payment_details'
+    };
+
+    EcwidAppwid.refreshConfig();
+
 }
-script.src = 'https://d35z3p2poghz10.cloudfront.net/ecwid-sdk/js/1.2.5/ecwid-app.js';
+script.src = 'https://d35z3p2poghz10.cloudfront.net/EcwidAppwid-sdk/js/1.2.5/EcwidAppwid-app.js';
 head.appendChild(script);
 
-ec.order = ec.order || {};
-ec.order.extraFields = ec.order.extraFields || {};
-
-ec.order.extraFields.nombre = {
-    'title': 'Nombre para la factura',
-    'textPlaceholder': 'Nombre para la factura',
-    'type': 'text',
-    'tip': 'Nombre o Consumidor final',
-    'required': true,
-    'checkoutDisplaySection': 'payment_details'
-};
-
-Ecwid.refreshConfig();
-
-ec.order.extraFields.nit = {
-    'title': 'Número de NIT',
-    'textPlaceholder': 'Package sign',
-    'type': 'text',
-    'tip': 'Ingrese el número de NIT',
-    'required': false,
-    'checkoutDisplaySection': 'payment_details'
-};
-
-Ecwid.refreshConfig();
-
-ec.order.extraFields.direccion = {
-    'title': 'Dirección',
-    'textPlaceholder': 'Package sign',
-    'type': 'text',
-    'tip': 'Ingrese la dirección para la factura',
-    'required': false,
-    'checkoutDisplaySection': 'payment_details'
-};
-
-Ecwid.refreshConfig();
