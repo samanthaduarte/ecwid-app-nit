2c83c4186143e3b898a7bbb9904f00726a944fda
samanthaduarte <samanthamdc97@gmail.com>
2020-04-18 00:25:40 -0600

    final

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ ec.order.extraFields = ec.order.extraFields || {};
 
 ec.order.extraFields.nombre = {
     'title': 'Nombre para la factura',
-    'textPlaceholder': 'Nombre o Consumidor Final',
+    'textPlaceholder': 'Nombre para la factura',
     'type': 'text',
-    'tip': ' ',
+    'tip': 'Nombre o Consumidor final',
     'required': true,
     'checkoutDisplaySection': 'payment_details'
 };
@@ -15,9 +15,9 @@ Ecwid.refreshConfig();
 
 ec.order.extraFields.nit = {
     'title': 'Número de NIT',
-    'textPlaceholder': 'Ingrese el número de NIT',
+    'textPlaceholder': 'Package sign',
     'type': 'text',
-    'tip': ' ',
+    'tip': 'Ingrese el número de NIT',
     'required': false,
     'checkoutDisplaySection': 'payment_details'
 };
@@ -26,9 +26,9 @@ Ecwid.refreshConfig();
 
 ec.order.extraFields.direccion = {
     'title': 'Dirección',
-    'textPlaceholder': 'Ingrese la dirección para la factura',
+    'textPlaceholder': 'Package sign',
     'type': 'text',
-    'tip': ' ',
+    'tip': 'Ingrese la dirección para la factura',
     'required': false,
     'checkoutDisplaySection': 'payment_details'
 };
